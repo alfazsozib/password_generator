65e2900057c92eba3c12fa337042ef9850170f55
alfazsozib <alfazsozib@gmail.com>
2025-02-01 01:55:44 +0600

    password generating

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -8,9 +8,23 @@ function App() {
   const [password, setPassword] = useState('')
 
 
-  const generatePassword=()=>{
+  const generatePassword = useCallback(()=>{
+      let pass = ""
+      let str = "ABCDEFGHIJKLMNOPQURSTUVWXYZabcdefghijklmnopqurstuvwxyz"
 
-  }
+      if(numberAllowed) str+= "0123456789"
+      if(charAllowed) str+= "!@#$%^&*()_+"
+
+      for(let i=1; i<length; i++){
+        const char = Math.floor(Math.random() * str.length + 1);
+        pass+= str.charAt(char)
+      }
+      setPassword(pass)
+  },[length, numberAllowed, charAllowed])
+
+  useEffect(()=>{
+    generatePassword();
+  },[length,numberAllowed,charAllowed])
 
   return (
     <>
